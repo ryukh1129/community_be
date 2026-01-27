25df12c97b8bb57f0538fac0fb361a4e978f830b
ryukh1129 <ryukh1129@gmail.com>
2026-01-27 12:19:21 +0900

    feat: 프로젝트 기초 구조 생성 / 핵심 엔터티 설계

---
+++ src/main/java/com/meta/community_be/file/domain/File.java
@@ -0,0 +1,27 @@
+package com.meta.community_be.file.domain;
+
+import com.meta.community_be.common.domain.TimeStamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "file")
+public class File extends TimeStamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String originalFileName;
+
+    @Column(nullable = false)
+    private String storedFilName;
+
+    @Column(nullable = false)
+    private String filePath;
+}
