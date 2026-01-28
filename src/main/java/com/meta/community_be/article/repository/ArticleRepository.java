75c451a6c33b1f06dec284f6da9313e30f21bfd4
ryukh1129 <ryukh1129@gmail.com>
2026-01-28 11:19:09 +0900

    feat: 게시글 CRUD 기본 기능 구현
    - 스프링시큐리티의 엔드포인트 접근권한 임시 전체 활성화
    - 기본 기능 POSTMAN 동작 테스트 완료

--- src/main/java/com/meta/community_be/article/repository/ArticleRepository.java
+++ src/main/java/com/meta/community_be/article/repository/ArticleRepository.java
@@ -3,5 +3,11 @@ package com.meta.community_be.article.repository;
 import com.meta.community_be.article.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ArticleRepository extends JpaRepository<Article,Long> {
+    List<Article> findAllByOrderByCreatedAtDesc();
+
+    Optional<Article> findByIdAndBoardId(Long id, Long boardId);
 }
